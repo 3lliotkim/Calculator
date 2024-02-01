17029a8a265147a53fe84c8f8d183ef1d12c3337
Elliot Kim <3lliotkim@gmail.com>
2024-01-31 20:43:07 -0500

    Completed GeoQuiz SnackBar

--- app/src/main/java/com/example/cs501_assignment1/MainActivity.kt
+++ app/src/main/java/com/example/cs501_assignment1/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.cs501_assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 import com.example.cs501_assignment1.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val textBank = listOf(Text(R.string.first),
-        Text(R.string.hello_world))
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_mideast, false))
 
     private var currentIndex = 0
 
@@ -19,15 +24,38 @@ class MainActivity : AppCompatActivity() {
             binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
 
-        binding.sayHello.setOnClickListener {
-            currentIndex = (currentIndex + 1) % textBank.size
-            updateText()
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(view,true)
+        }
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(view,false)
         }
 
-        updateText()
+        binding.nextButton.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
     }
-    private fun updateText() {
-        val textResId = textBank[currentIndex].textResId
-        binding.textView.setText(textResId)
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(view: View, userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        val messageResIdy = if (userAnswer == correctAnswer) {
+            R.string.correct_snack
+        } else {
+            R.string.incorrect_snack
+        }
+        //Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        Snackbar.make(view, messageResIdy, Snackbar.LENGTH_SHORT).show();
     }
 }
